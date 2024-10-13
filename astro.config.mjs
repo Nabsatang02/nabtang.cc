affff34139f0d61c6d927ba089fd603fd8459070
Nabsatang02 <Nabtang@siamkane.com>
2024-10-13 10:48:24 +0700

    Add Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://nabtang.cc',
+  integrations: [tailwind(), react(), sitemap()]
 });
